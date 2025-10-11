f38132db0008f463361615a5beaa9de5316dc5da
Lenny <plax0099@gmail.com>
2025-10-11 15:46:48 -0700

    Primer commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Gracias por contactarte. Te responderé pronto 💬");
+    this.reset();
+});
