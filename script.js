42082e7fa0859e89e7d178c80ad339d6b9452069
Lenny0099 <102004838+Lenny0099@users.noreply.github.com>
2025-10-11 17:21:36 -0700

    2
    segundo cambio

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
     alert("Gracias por contactarte. Te responderé pronto 💬");
     this.reset();
 });
+
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    if (navLinks.style.right === "0px") {
+        navLinks.style.right = "-100%";
+    } else {
+        navLinks.style.right = "0px";
+    }
+});
+
+
+ScrollReveal().reveal('.section', {
+  duration: 1200,
+  distance: '60px',
+  origin: 'bottom',
+  easing: 'ease-in-out',
+  reset: true // Si quieres que se repita la animación al hacer scroll
+});
+
+
+AOS.init({
+  duration: 1200,
+  once: true
+});
